1f013d1811862901de18c33e4807e4fdfb4f0cf4
Jakub Żelichowski <zeleg58@gmail.com>
2021-01-12 15:06:01 +0100

    przygotowanie widoku szczegolowego pojedynczego wpisu

--- blog/src/app/components/blog-item-details/blog-item-details.component.ts
+++ blog/src/app/components/blog-item-details/blog-item-details.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from '../services/data.service';
+import {ActivatedRoute} from '@angular/router';
+
 
 @Component({
-  selector: 'blog-item-details',
+  selector: 'app-blog-item-details',
   templateUrl: './blog-item-details.component.html',
   styleUrls: ['./blog-item-details.component.css']
 })
 export class BlogItemDetailsComponent implements OnInit {
+public image = '';
+  public text: string;
+  public id: number;
+
+  constructor(private dataService: DataService, private route: ActivatedRoute) {
+  }
+
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  	let id: string;
+  	this.route.paramMap
+    	   .subscribe(params => {
+      	   id = params.get('id');
+    	});
+  	if (id) {
+    	   this.dataService.getById(id).subscribe(res => {
+      	   this.image = res['image'];
+      	   this.text = res['text'];
+    	});
+  	} else {
+    	   this.id = 1;
+  	}
+    }
   }
 
-}
--- blog/src/app/services/data.service.ts
+++ blog/src/app/services/data.service.ts
@@ -1,15 +1,31 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
 
 @Injectable()
 export class DataService {
 
-  private url = 'https://jsonplaceholder.typicode.com/photos';
+  private url = 'https://blogtai.herokuapp.com/api/posts';
+  private url = 'https://blogtai.herokuapp.com';
 
   constructor(private http: HttpClient) {
   }
+  constructor(private http: HttpClient) {
+  }
+
 
   getAll() {
     return this.http.get(this.url);
   }
+  getAll() {
+  	return this.http.get(this.url + '/api/posts/');
+  }
+
+  getById(id) {
+            return this.http.get(this.url + '/api/posts/' + id);
+  }
+
+
 }
